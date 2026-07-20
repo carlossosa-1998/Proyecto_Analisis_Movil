2ad0e476b672cd0d7b556dfa8aeb0af5246b3f60
carlossosa-1998 <113155435+carlossosa-1998@users.noreply.github.com>
2026-07-20 12:59:25 -0600

    Set base path for GitHub Pages
    Added base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
@@ -17,6 +16,9 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  // Ruta base necesaria para que GitHub Pages reconozca los archivos estáticos
+  base: '/Proyecto_Analisis_Web/',
+  
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
